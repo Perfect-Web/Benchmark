4cf3be66b8edfb2c4aa21404e33efa2b0dff2cfa
Alin Jurj <office@perfectweb.ro>
2014-06-18 20:34:45 +0300

    changed table width

--- Benchmark.php
+++ Benchmark.php
@@ -1,144 +1,183 @@
 <?php
-	class Benchmark {
-		private $sections = array();
 
-		function __construct() {
-			$this->sections['total'] = array();
-			$this->sections['total'][] = array(
-				'time_start' => microtime(true),
-				'time_stop'  => null,
+class Benchmark
+{
+
+	private $sections = array();
+
+	function __construct()
+	{
+
+		$this->sections['total'] = array();
+		$this->sections['total'][] = array(
+			'time_start'   => microtime(true),
+			'time_stop'    => null,
+			'memory_start' => memory_get_usage(),
+			'memory_stop'  => null
+		);
+	}
+
+	/**
+	 * Returns a human readable time (ms, s, m)
+	 */
+	function units($type, $value)
+	{
+
+		switch ($type) {
+			case 'time':
+				$value = floatval($value);
+				if ($value <= 1) {
+					$unit = 'ms';
+					$amount = intval($value * 1000);
+				}
+				else if ($value <= 60) {
+					$unit = 's';
+					$amount = round($value, 2);
+				}
+				else if ($value <= 3600) {
+					$unit = 'm';
+					$amount = round($value / 60, 2);
+				}
+
+				return "$amount $unit";
+			case 'bytes':
+				$sizes = array(
+					' B',
+					'kB',
+					'MB',
+					'GB',
+					'TB',
+					'PB',
+					'EB',
+					'ZB',
+					'YB'
+				);
+				for ($i = 0; abs($value) > 1024 && isset($sizes[$i + 1]); ++$i) $value /= 1024;
+
+				return sprintf("%3.0f %2s", $value, $sizes[$i]);
+		}
+	}
+
+	function start($section)
+	{
+
+		if ($section == 'total') throw new Exception('a total measurement is already being made.');
+		$this->calculated = false;
+		if (!isset($this->sections[$section])) {
+			$this->sections[$section] = array();
+			$this->sections[$section][] = array(
+				'time_start'   => microtime(true),
+				'time_stop'    => null,
 				'memory_start' => memory_get_usage(),
-				'memory_stop' => null
+				'memory_stop'  => null
 			);
 		}
-
-		/**
-		 * Returns a human readable time (ms, s, m)
-		 */
-		function units($type, $value) {
-			switch($type) {
-				case 'time':
-					$value = floatval($value);
-					if( $value <= 1 ) {
-						$unit = 'ms';
-						$amount = intval($value * 1000);
-					} else if( $value <= 60 ) {
-						$unit = 's';
-						$amount = round($value, 2);
-					} else if( $value <= 3600 ) {
-						$unit = 'm';
-						$amount = round($value / 60, 2);
-					}
-					return "$amount $unit";
-				case 'bytes':
-					$sizes = array(' B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
-					for ($i = 0; abs($value) > 1024 && isset($sizes[$i+1]); ++$i)
-					  $value /= 1024;
-					return sprintf("%3.0f %2s", $value, $sizes[$i]);
-			}
-		}
-
-		function start($section) {
-			if( $section == 'total' )
-				throw new Exception('a total measurement is already being made.');
-			$this->calculated = false;
-			if( !isset($this->sections[$section]) ) {
-				$this->sections[$section] = array();
-				$this->sections[$section][] = array(
-					'time_start' => microtime(true),
-					'time_stop'  => null,
-					'memory_start' => memory_get_usage(),
-					'memory_stop' => null
-				);
-			} else {
-				$this->sections[$section][] = array(
-					'time_start' => microtime(true),
-					'time_stop'  => null,
-					'memory_start' => memory_get_usage(),
-					'memory_stop' => null
-				);
-			}
-		}
-
-		function stop($section) {
-			$this->calculated = false;
-			$this->sections[$section][count($this->sections[$section]) - 1]['time_stop'] = microtime(true);
-			$this->sections[$section][count($this->sections[$section]) - 1]['memory_stop'] = memory_get_usage();
-		}
-
-		function calculate_median($arr) {
-			sort($arr);
-			$count = count($arr); //total numbers in array
-			$middleval = floor(($count-1)/2); // find the middle value, or the lowest middle value
-			if($count % 2) { // odd number, middle is the median
-				$median = $arr[$middleval];
-			} else { // even number, calculate avg of 2 medians
-				$low = $arr[$middleval];
-				$high = $arr[$middleval+1];
-				$median = (($low+$high)/2);
-			}
-			return $median;
-		}
-
-		function avg_abs_deviation($arr, $center) {
-			$sum = 0;
-			$count = 0;
-			foreach($arr as $value) {
-				$count++;
-				$sum += abs($value - $center);
-			}
-			return $sum / $count;
-		}
-
-		private function calculate($name) {
-			$count = 0;
-			$time = 0;
-			$memory = 0;
-			$time_deltas = array();
-			$memory_deltas = array();
-			foreach($this->sections[$name] as $index => $section) {
-				$count += 1;
-				$time += $section['time_stop'] - $section['time_start'];
-				$time_deltas[] = $section['time_stop'] - $section['time_start'];
-				$memory += $section['memory_stop'] - $section['memory_start'];
-				$memory_deltas[] = $section['memory_stop'] - $section['memory_start'];
-			}
-			$median_time = $this->calculate_median($time_deltas);
-			$median_time_deviation = $this->avg_abs_deviation($time_deltas, $median_time);
-			$mean_time_deviation = $this->avg_abs_deviation($time_deltas, $time / $count);
-			$median_memory = $this->calculate_median($memory_deltas);
-			$median_memory_deviation = $this->avg_abs_deviation($memory_deltas, $median_memory);
-			$this->sections[$name]['totals'] = array(
-				'count' => $count,
-				'mean_time' => $time / $count,
-				'median_time' => $median_time,
-				'median_time_deviation' => $median_time_deviation,
-				'mean_time_deviation' => $mean_time_deviation,
-				'mean_memory' => $memory / $count,
-				'median_memory' => $median_memory,
-				'median_memory_deviation' => $median_memory_deviation,
-				'time' => $time,
-				'memory' => $memory
+		else {
+			$this->sections[$section][] = array(
+				'time_start'   => microtime(true),
+				'time_stop'    => null,
+				'memory_start' => memory_get_usage(),
+				'memory_stop'  => null
 			);
 		}
+	}
 
-		function display($section = null) {
-			if( !$this->calculated ) {
-				$this->stop('total');
-				foreach($this->sections as $name => $section) {
-					$this->calculate($name);
+	function stop($section)
+	{
+
+		$this->calculated = false;
+		$this->sections[$section][count($this->sections[$section]) - 1]['time_stop'] = microtime(true);
+		$this->sections[$section][count($this->sections[$section]) - 1]['memory_stop'] = memory_get_usage();
+	}
+
+	function calculate_median($arr)
+	{
+
+		sort($arr);
+		$count = count($arr); //total numbers in array
+		$middleval = floor(($count - 1) / 2); // find the middle value, or the lowest middle value
+		if ($count % 2) { // odd number, middle is the median
+			$median = $arr[$middleval];
+		}
+		else { // even number, calculate avg of 2 medians
+			$low = $arr[$middleval];
+			$high = $arr[$middleval + 1];
+			$median = (($low + $high) / 2);
+		}
+
+		return $median;
+	}
+
+	function avg_abs_deviation($arr, $center)
+	{
+
+		$sum = 0;
+		$count = 0;
+		foreach ($arr as $value) {
+			$count++;
+			$sum += abs($value - $center);
+		}
+
+		return $sum / $count;
+	}
+
+	private function calculate($name)
+	{
+
+		$count = 0;
+		$time = 0;
+		$memory = 0;
+		$time_deltas = array();
+		$memory_deltas = array();
+		foreach ($this->sections[$name] as $index => $section) {
+			$count += 1;
+			$time += $section['time_stop'] - $section['time_start'];
+			$time_deltas[] = $section['time_stop'] - $section['time_start'];
+			$memory += $section['memory_stop'] - $section['memory_start'];
+			$memory_deltas[] = $section['memory_stop'] - $section['memory_start'];
+		}
+		$median_time = $this->calculate_median($time_deltas);
+		$median_time_deviation = $this->avg_abs_deviation($time_deltas, $median_time);
+		$mean_time_deviation = $this->avg_abs_deviation($time_deltas, $time / $count);
+		$median_memory = $this->calculate_median($memory_deltas);
+		$median_memory_deviation = $this->avg_abs_deviation($memory_deltas, $median_memory);
+		$this->sections[$name]['totals'] = array(
+			'count'                   => $count,
+			'mean_time'               => $time / $count,
+			'median_time'             => $median_time,
+			'median_time_deviation'   => $median_time_deviation,
+			'mean_time_deviation'     => $mean_time_deviation,
+			'mean_memory'             => $memory / $count,
+			'median_memory'           => $median_memory,
+			'median_memory_deviation' => $median_memory_deviation,
+			'time'                    => $time,
+			'memory'                  => $memory
+		);
+	}
+
+	function display($section = null)
+	{
+
+		if (!$this->calculated) {
+			$this->stop('total');
+			foreach ($this->sections as $name => $section) {
+				$this->calculate($name);
+			}
+			function cmp($a, $b)
+			{
+
+				if ($a['totals']['time'] == $b['totals']['time']) {
+					return 0;
 				}
-				function cmp($a, $b) {
-					if($a['totals']['time'] == $b['totals']['time']) {
-						return 0;
-					}
-					return ($a['totals']['time'] > $b['totals']['time']) ? -1 : 1;
-				}
-				uasort($this->sections, 'cmp');
-				$this->calculated = true;
+
+				return ($a['totals']['time'] > $b['totals']['time']) ? -1 : 1;
 			}
 
-			if (php_sapi_name() != 'cli'){
+			uasort($this->sections, 'cmp');
+			$this->calculated = true;
+		}
+
+		if (php_sapi_name() != 'cli') {
 
 			echo <<< EOT
 <table cellpadding="2" cellspacing="2" style="font-family: courier;">
@@ -165,11 +204,11 @@
 		<th>deviation</th>
 	</tr>
 EOT;
-			if( count($this->sections) == 1 ) {
+			if (count($this->sections) == 1) {
 				echo "<tr><td colspan='2'></td><td>|</td><td colspan='8' align='center'><em>No Data</em></td></tr>";
 			}
-			foreach($this->sections as $name => $section) {
-				if( $name == 'total' ) {
+			foreach ($this->sections as $name => $section) {
+				if ($name == 'total') {
 					continue;
 				}
 				echo <<< EOR
@@ -196,89 +235,89 @@ EOR;
 			echo "</tr>";
 			echo '</table>';
 
+		}
+		else {
+
+			$table = new Zend\Text\Table\Table(array(
+				                                   'columnWidths' => array(
+					                                   50,
+					                                   3,
+					                                   1,
+					                                   15,
+					                                   15,
+					                                   15,
+					                                   15,
+					                                   1,
+					                                   15,
+					                                   15,
+					                                   15,
+					                                   15
+				                                   )
+			                                   ));
+
+			if (count($this->sections) == 1) {
+				$table->appendRow('No Data');
 			}
-			else{
+			else {
 
-				$table = new Zend\Text\Table\Table(array(
-					                                   'columnWidths' => array(
-						                                   40,
-						                                   2,
-						                                   2,
-						                                   20,
-						                                   20,
-						                                   20,
-						                                   20,
-						                                   2,
-						                                   20,
-						                                   20,
-						                                   20,
-						                                   20
-					                                   )
-				                                   ));
+				$table->appendRow(array(
+					                  'Section',
+					                  '#',
+					                  '',
+					                  'Time',
+					                  '',
+					                  '',
+					                  '',
+					                  '',
+					                  'Memory',
+					                  '',
+					                  '',
+					                  ''
+				                  ));
 
-				if (count($this->sections) == 1) {
-					$table->appendRow('No Data');
-				}
-				else {
-
-					$table->appendRow(array(
-						                  'section',
-						                  ' # ',
-						                  ' | ',
-						                  'Time',
-						                  '   ',
-						                  '   ',
-						                  '   ',
-						                  ' | ',
-						                  'Memory',
-						                  '   ',
-						                  '   ',
-						                  '   '
-					                  ));
-
-					$table->appendRow(array(
-						                  '    ',
-						                  '    ',
-						                  ' | ',
-						                  'total',
-						                  'mean',
-						                  'median',
-						                  'deviation',
-						                  ' | ',
-						                  'total',
-						                  'mean',
-						                  'median',
-						                  'deviation'
-					                  ));
+				$table->appendRow(array(
+					                  '',
+					                  '',
+					                  '',
+					                  'total',
+					                  'mean',
+					                  'median',
+					                  'deviation',
+					                  '',
+					                  'total',
+					                  'mean',
+					                  'median',
+					                  'deviation'
+				                  ));
 
 
-					foreach ($this->sections as $name => $section) {
+				foreach ($this->sections as $name => $section) {
 
-						if ($name == 'total') {
-							continue;
-						}
-
-						$table->appendRow(array(
-							$name,
-							$section['totals']['count'],
-						    ' | ',
-						    $this->units('time', $section['totals']['time']),
-						    $this->units('time', $section['totals']['mean_time']),
-						    $this->units('time', $section['totals']['median_time']),
-						    '+/-' . $this->units('time', $section['totals']['median_time_deviation']),
-						    ' | ',
-						    $this->units('bytes', $section['totals']['memory']),
-						    $this->units('bytes', $section['totals']['mean_memory']),
-						    $this->units('bytes', $section['totals']['median_memory']),
-						    '+/-' . $this->units('bytes', $section['totals']['median_memory_deviation'])
-						));
-
+					if ($name == 'total') {
+						continue;
 					}
 
+					$table->appendRow(array(
+						                  $name,
+						                  $section['totals']['count'].'',
+						                  '',
+						                  $this->units('time', $section['totals']['time']),
+						                  $this->units('time', $section['totals']['mean_time']),
+						                  $this->units('time', $section['totals']['median_time']),
+						                  '+/-'.$this->units('time', $section['totals']['median_time_deviation']),
+						                  '',
+						                  $this->units('bytes', $section['totals']['memory']),
+						                  $this->units('bytes', $section['totals']['mean_memory']),
+						                  $this->units('bytes', $section['totals']['median_memory']),
+						                  '+/-'.$this->units('bytes', $section['totals']['median_memory_deviation'])
+					                  ));
+
 				}
 
-				echo $table;
-
 			}
+
+			echo $table;
+
 		}
 	}
+}
