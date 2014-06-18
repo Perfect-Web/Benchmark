ce94070cb3c03c98d55987d706c0352d0e1dbc50
Alin Jurj <office@perfectweb.ro>
2014-06-18 19:32:20 +0300

    added zend text table for console output

--- Benchmark.php
+++ Benchmark.php
@@ -1,7 +1,7 @@
 <?php
 	class Benchmark {
 		private $sections = array();
-		
+
 		function __construct() {
 			$this->sections['total'] = array();
 			$this->sections['total'][] = array(
@@ -122,7 +122,7 @@
 			);
 		}
 
-		function display($section = null) {			
+		function display($section = null) {
 			if( !$this->calculated ) {
 				$this->stop('total');
 				foreach($this->sections as $name => $section) {
@@ -137,6 +137,9 @@
 				uasort($this->sections, 'cmp');
 				$this->calculated = true;
 			}
+
+			if (php_sapi_name() != 'cli'){
+
 			echo <<< EOT
 <table cellpadding="2" cellspacing="2" style="font-family: courier;">
 	<tr style="font-family: verdana;">
@@ -186,11 +189,81 @@ EOT;
 				</tr>
 EOR;
 			}
-			echo "<tr><td colspan='12'><hr/></td>";	
+			echo "<tr><td colspan='12'><hr/></td>";
 			echo "<tr><td colspan='2'>total</td>";
 			echo "<td>|</td><td colspan='4'>{$this->units('time', $this->sections['total']['totals']['time'])}</td>";
 			echo "<td>|</td><td colspan='4'>{$this->units('bytes', $this->sections['total']['totals']['memory'])}</td>";
 			echo "</tr>";
 			echo '</table>';
+
+			}
+			else{
+
+				$table = new Zend\Text\Table\Table();
+
+				if (count($this->sections) == 1) {
+					$table->appendRow('No Data');
+				}
+				else {
+
+					$table->appendRow(array(
+						                  'section',
+						                  ' # ',
+						                  ' | ',
+						                  'Time',
+						                  '   ',
+						                  '   ',
+						                  '   ',
+						                  ' | ',
+						                  'Memory',
+						                  '   ',
+						                  '   ',
+						                  '   ',
+					                  ));
+
+					$table->appendRow(array(
+						                  '    ',
+						                  '    ',
+						                  ' | ',
+						                  'total',
+						                  'mean',
+						                  'median',
+						                  'deviation',
+						                  ' | ',
+						                  'total',
+						                  'mean',
+						                  'median',
+						                  'deviation',
+					                  ));
+
+
+					foreach ($this->sections as $name => $section) {
+
+						if ($name == 'total') {
+							continue;
+						}
+
+						$table->appendRow(array(
+							$name,
+							$section['totals']['count'],
+						    ' | ',
+						    $this->units('time', $section['totals']['time']),
+						    $this->units('time', $section['totals']['mean_time']),
+						    $this->units('time', $section['totals']['median_time']),
+						    '+/-' . $this->units('time', $section['totals']['median_time_deviation']),
+						    ' | ',
+						    $this->units('bytes', $section['totals']['memory']),
+						    $this->units('bytes', $section['totals']['mean_memory']),
+						    $this->units('bytes', $section['totals']['median_memory']),
+						    '+/-' . $this->units('bytes', $section['totals']['median_memory_deviation'])
+						));
+
+					}
+
+				}
+
+				echo $table;
+
+			}
 		}
 	}
